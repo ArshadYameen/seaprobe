c2dbffec41999a6cc5fd43645bbcafafe5e3552e
ArshadYameen <arshadyameen666@gmail.com>
2025-08-07 21:28:52 +0100

    Initializing Base Structure

---
+++ src/main/java/com/probe/seaprobe/SeaprobeApplication.java
@@ -0,0 +1,13 @@
+package com.probe.seaprobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaprobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaprobeApplication.class, args);
+	}
+
+}
